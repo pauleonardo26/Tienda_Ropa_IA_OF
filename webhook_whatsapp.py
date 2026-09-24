0484e1a38cf94aaf5e86c60c20f106c54484f77d
pauleonardo26 <113878100+pauleonardo26@users.noreply.github.com>
2026-09-24 00:56:43 -0500

    Fix indentation issue in webhook_whatsapp.py

--- webhook_whatsapp.py
+++ webhook_whatsapp.py
@@ -2666,6 +2666,7 @@ def recibir_mensaje():
                         "Ejemplo:\n"
                         "1 legging negro talla 4"
                     )
+                 )
 
                 return "EVENT_RECEIVED", 200
 
